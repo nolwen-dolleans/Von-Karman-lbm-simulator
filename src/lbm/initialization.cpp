bc4418aaeaff4274e62007c70d03bd48d037437d
Gatho <61396959+theob55@users.noreply.github.com>
2026-04-25 12:41:16 +0200

    Changed poiseuille function
    Removed init_cond_velocity_0_density_1 (unused)
    rearranged setup_init_state_global_poiseuille_profile without if

--- src/lbm/initialization.cpp
+++ src/lbm/initialization.cpp
@@ -7,19 +7,18 @@
 
 #include <lbm/physics.hpp>
 
-void init_cond_velocity_0_density_1(Mesh* mesh) {
-  assert(mesh != NULL);
+// void init_cond_velocity_0_density_1(Mesh* mesh) {
+//   assert(mesh != NULL);
 
-  // Loop on all cells
-  for (size_t i = 0; i < mesh->width; i++) {
-    for (size_t j = 0; j < mesh->height; j++) {
-		auto cell = Mesh_get_cell(mesh, i, j);
-      for (size_t k = 0; k < DIRECTIONS; k++) {
-		  cell[k] = equil_weight[k];
-      }
-    }
-  }
-}
+//   // Loop on all cells
+//   for (size_t i = 0; i < mesh->width; i++) {
+//     for (size_t j = 0; j < mesh->height; j++) {
+//       for (size_t k = 0; k < DIRECTIONS; k++) {
+//         Mesh_get_cell(mesh, i, j)[k] = equil_weight[k];
+//       }
+//     }
+//   }
+// }
 
 void setup_init_state_circle_obstacle(Mesh* mesh, lbm_mesh_type_t* mesh_type, const lbm_comm_t* mesh_comm) {
   // Loop on nodes
@@ -43,20 +42,24 @@ void setup_init_state_global_poiseuille_profile(Mesh* mesh, lbm_mesh_type_t* mes
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
   // Apply Poiseuille distribution for all nodes except on top/bottom border
-  for (size_t i = 0; i < mesh->width; i++) {
-    for (size_t j = 0; j < mesh->height; j++) {
-		v[0]                 = helper_compute_poiseuille(j + mesh_comm->y, MESH_HEIGHT);
-		lbm_mesh_cell_t cell = Mesh_get_cell(mesh, i, j);
+  for (size_t j = 0; j < mesh->height; j++) {
+    v[0] = helper_compute_poiseuille(j + mesh_comm->y, MESH_HEIGHT);
+    double v2 = get_vect_norm_2(v, v);
+
+    for (size_t k = 0; k < DIRECTIONS; k++) {
+      // Compute equilibrium
+      lbm_mesh_cell_t cell0 = Mesh_get_cell(mesh, 0, j);
+      lbm_mesh_cell_t cell1 = Mesh_get_cell(mesh, 1, j);
+      cell0[k]              = compute_equilibrium_profile(v, rho, k, v2);
+      cell1[k]              = compute_equilibrium_profile(v, rho, k, v2);
+      // Mark as standard fluid
+      *(lbm_cell_type_t_get_cell(mesh_type, 0, j)) = CELL_FUILD;
+      *(lbm_cell_type_t_get_cell(mesh_type, 1, j)) = CELL_FUILD;
+    }
+
+    for (size_t i = 2; i < mesh->width; i++) {
       for (size_t k = 0; k < DIRECTIONS; k++) {
-        // Compute equilibrium
-        
-        cell[k]              = compute_equilibrium_profile(v, rho, k, 0);
-        // Mark as standard fluid
-        *(lbm_cell_type_t_get_cell(mesh_type, i, j)) = CELL_FUILD;
-        // This is a try to init the fluid with a null speed except on the left border.
-        if (i > 1) {
-          Mesh_get_cell(mesh, i, j)[k] = equil_weight[k];
-        }
+        Mesh_get_cell(mesh, i, j)[k] = equil_weight[k];
       }
     }
   }
@@ -85,7 +88,8 @@ void setup_init_state_border(Mesh* mesh, lbm_mesh_type_t* mesh_type, const lbm_c
     for (size_t i = 0; i < mesh->width; i++) {
       for (size_t k = 0; k < DIRECTIONS; k++) {
         // Compute equilibrium.
-        Mesh_get_cell(mesh, i, 0)[k] = compute_equilibrium_profile(v, rho, k, 0);
+		    double v2 = get_vect_norm_2(v, v);
+        Mesh_get_cell(mesh, i, 0)[k] = compute_equilibrium_profile(v, rho, k, v2);
         // Mark as bounce back
         *(lbm_cell_type_t_get_cell(mesh_type, i, 0)) = CELL_BOUNCE_BACK;
       }
@@ -97,7 +101,8 @@ void setup_init_state_border(Mesh* mesh, lbm_mesh_type_t* mesh_type, const lbm_c
     for (size_t i = 0; i < mesh->width; i++) {
       for (size_t k = 0; k < DIRECTIONS; k++) {
         // Compute equilibrium.
-        Mesh_get_cell(mesh, i, mesh->height - 1)[k] = compute_equilibrium_profile(v, rho, k, 0);
+		double v2 = get_vect_norm_2(v, v);
+        Mesh_get_cell(mesh, i, mesh->height - 1)[k] = compute_equilibrium_profile(v, rho, k, v2);
         // Mark as bounce back
         *(lbm_cell_type_t_get_cell(mesh_type, i, mesh->height - 1)) = CELL_BOUNCE_BACK;
       }
